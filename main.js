66ca93337007232888a2180873237dcecd4e4eeb
UnseenAlpha <77725802+UnseenAlpha@users.noreply.github.com>
2021-09-03 14:46:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 1;
+ctx.rect(190, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(300, 210, 40, 0 ,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "#F6BE00";
+ctx.lineWidth = 5;
+ctx.arc(350, 250, 40, 0 ,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(400, 210, 40, 0 ,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(450, 250, 40, 0 ,2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(500, 210, 40, 0 ,2*Math.PI);
+ctx.stroke();
